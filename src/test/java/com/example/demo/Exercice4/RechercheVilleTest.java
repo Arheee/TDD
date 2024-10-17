61815ea406ee8144cb022f19ab968ced56201012
Arheee <justinewallet1@gmail.com>
2024-10-17 11:04:53 +0200

    Exercice4 - Tous les tests échouent sauf le 1 (deja fait), car les fonctionnalités ne sont pas encore implémentées

--- src/test/java/com/example/demo/Exercice4/RechercheVilleTest.java
+++ src/test/java/com/example/demo/Exercice4/RechercheVilleTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.webjars.NotFoundException;
 
+import java.util.List;
+
 public class RechercheVilleTest {
 
 
@@ -18,7 +20,58 @@ public class RechercheVilleTest {
         Assert.assertThrows(NotFoundException.class, ()-> {
             rechercheVille.rechercher("A");
         });
-
     }
 
+    @Test
+    @DisplayName("Texte de recherche => a 2 char, renvois les noms de villes qui match")
+    public void whenTextIs2OrMoreChar_thenReturnMatchingCities(){
+        //Arrange
+        RechercheVille rechercheVille = new RechercheVille();
+        //Act
+        List<String> result = rechercheVille.rechercher("va");
+
+        //Assert
+        Assert.assertTrue(result.contains("Valence"));
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    @DisplayName("insensible à la casse")
+    public void whenTextIsCaseInsensitive_thenReturnMatchingCities(){
+        // Arrange
+        RechercheVille rechercheVille = new RechercheVille();
+
+        // Act
+        List<String> result = rechercheVille.rechercher("va");
+
+        // Assert
+        Assert.assertTrue(result.contains("Valence"));
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    @DisplayName("Recherche lorsque le texte n'est qu'une partie d'un nom de ville")
+    public void whenTextIsSubstring_thenReturnMatchingCities(){
+        // Arrange
+        RechercheVille rechercheVille = new RechercheVille();
+
+        // Act
+        List<String> result = rechercheVille.rechercher("ape");
+
+        // Assert
+        Assert.assertTrue(result.contains("Budapest"));
+        Assert.assertEquals(1, result.size());
+    }
+    @Test
+    @DisplayName("* renvoie tous les noms de ville")
+    public void whenTextIsAsterisk_thenReturnAllCities() {
+        // Arrange
+        RechercheVille rechercheVille = new RechercheVille();
+
+        // Act
+        List<String> result = rechercheVille.rechercher("*");
+
+        // Assert
+        Assert.assertEquals(15, result.size());
+    }
 }
